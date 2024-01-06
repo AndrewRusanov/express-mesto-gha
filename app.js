c1c199323b785da138991945480a568af28d4170
AndrewRusanov <rusanow.andrej2018@yandex.ru>
2024-01-06 15:01:10 +0300

    change inport express lib

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require("express");
+import express from "express";
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.listen(PORT, ()=>{
-  console.log(`App listening on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
