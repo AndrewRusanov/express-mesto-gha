78cd9539006fc980502148bc8e965d50663dbdef
AndrewRusanov <rusanow.andrej2018@yandex.ru>
2024-01-07 18:01:50 +0300

    feat: create cardScheme

---
+++ models/Card.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+
+const cardScheme = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minlength: [2, "Минимальная длина поля name: 2 символа"],
+      maxlength: [30, "Максимальная длина поля name: 30 символов"],
+    },
+    link: {
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    likes: [{ type: mongoose.Schema.Types.ObjectId, default: [] }],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+  },
+  { versionKey: false }
+);
+
+export default mongoose.model("card", cardScheme);
