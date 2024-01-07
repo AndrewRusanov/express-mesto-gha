9884d8e9ca16b4b4dc02a25aabffc2cfdccf70ba
AndrewRusanov <rusanow.andrej2018@yandex.ru>
2024-01-07 18:13:48 +0300

    feat: made a temporary authorization solution

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  req.user = {
+    _id: "659a517f79caad48ae078cc9",
+  };
+  next();
+});
 
 mongoose.connect("mongodb://127.0.0.1:27017/mestodb");
 
